21a7343247646321c5ff3c832b91a64a50fa6ff8
Gragit <gme2468@gmail.com>
2020-11-22 23:01:10 +0800

    added JAWSDB code

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,25 @@
 var mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "127.0.0.1",
     port: 3306,
     user: "root",
     password: "",
     database: "burgers_db"
   });
+}
 
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+  connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-  module.exports = connection;
+module.exports = connection;
